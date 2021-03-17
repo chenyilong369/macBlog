84952df14ef060b9fe8c9ba896df10f08a0c621a
chenyilong <1456014766@qq.com>
2021-03-17 10:30:18 +0800

    init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home : React.FC<any> = (props) => {
+  return (
+    <div>
+      <img src="image/home.jpg" width="100 vw" />
+    </div>
+  )
+}
+
+export default Home
